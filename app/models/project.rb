2c2feadc68336f7a0927dd7943155e27b21837d2
sriram0601 <sriramachandran.2019@gmail.com>
2023-04-15 09:31:35 +0530

    Team and form style

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+  has_rich_text :description
+
+  # ====== Validations ======
+  validates :description, presence: true
+  validates :title, presence: true
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( main.css css/core.css css/demo.css css/pages/page-account-settings.css css/pages/page-auth.css css/pages/page-icons.css css/pages/page-misc.css css/theme-default.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
